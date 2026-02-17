209ae4e86de42b9c6f370c1a63999a887ab016ac
Yaniv Joseph <yaniv.joseph@weka.io>
2026-02-17 17:22:33 +0200

    fixup! Add CX8 to HCA table
    CX9/BF4 place-holder

--- providers/mlx5/mlx5.c
+++ providers/mlx5/mlx5.c
@@ -82,10 +82,12 @@ static const struct verbs_match_ent hca_table[] = {
 	HCA(MELLANOX, 0x101e),	/* ConnectX family mlx5Gen Virtual Function */
 	HCA(MELLANOX, 0x1021),  /* ConnectX-7 */
 	HCA(MELLANOX, 0x1023),  /* ConnectX-8 */
+	/*HCA(MELLANOX, 0x1025),*/  /* ConnectX-9 not enabled */
 	HCA(MELLANOX, 0xa2d2),	/* BlueField integrated ConnectX-5 network controller */
 	HCA(MELLANOX, 0xa2d3),	/* BlueField integrated ConnectX-5 network controller VF */
 	HCA(MELLANOX, 0xa2d6),  /* BlueField-2 integrated ConnectX-6 Dx network controller */
 	HCA(MELLANOX, 0xa2dc),  /* BlueField-3 integrated ConnectX-7 network controller */
+	/*HCA(MELLANOX, 0xa2df),*/  /* BlueField-4 integrated ConnectX-9 network controller */
 	{}
 };
 
